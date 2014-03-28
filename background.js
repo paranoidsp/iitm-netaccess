f4a151bf07696f5df516428c1f66ffecaad808d3
Karthikeya Viswanath <viswanathkarthikeya@gmail.com>
2014-03-28 06:21:17 -0100

    Moved mem to background

--- background.js
+++ background.js
@@ -1,2 +1,36 @@
-chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
-chrome.browserAction.setBadgeText({text: 'Arbit'});
+
+function getAccess(){
+    //$.ajax({url: "net"})
+    var req = new XMLHttpRequest();
+    req.onreadystatechange = function() {
+        if (req.readyState == 4) {
+            updateBadge(getMem(req.responseText));
+            //updateBadge("Hello");//getMem(req.responseText));
+            //alert(req.responseText);
+        }
+    }
+    req.open("GET", 'https://netaccess.iitm.ac.in/', true);
+    req.send(null);
+}
+
+function getMem(txt){
+    var a = String(txt).match(/\d*\.\d*\sMB/);
+    var b = String(a);
+    //var b = a.substring(0,4);
+    if(b.substring(3,4) == "."){
+        return b.substring(0,3) ;}
+    else{
+        return b.substring(0,4) ;}
+}
+
+function updateBadge(txt){
+    chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
+    chrome.browserAction.setBadgeText({text:String(txt)});
+}
+
+//chrome.browserAction.onClicked.addListener(getAccess);
+getAccess();
+
+
+
+
